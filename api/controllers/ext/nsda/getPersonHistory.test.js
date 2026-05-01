e7ab6496e1dad430d3ed7b0df97a1a5838375f41
Chris Palmer <palmer@tabroom.com>
2026-04-30 20:50:14 -0400

    What are there

--- api/controllers/ext/nsda/getPersonHistory.test.js
+++ api/controllers/ext/nsda/getPersonHistory.test.js
@@ -5,27 +5,25 @@ import { testUserAPIKey } from '../../../../tests/testFixtures';
 
 const authHeader = Buffer.from(`69:${testUserAPIKey.value}`).toString('base64');
 
+// eslint-disable-next-line jest/no-disabled-tests
 describe.skip('Person History', () => {
 
 	it('Returns history for a person', async () => {
+		// Victim of the day!
+		const res = await request(server)
+			.get(`/v1/ext/nsda/history?nsda_id=4`)
+			.set('Accept', 'application/json')
+			.set('Authorization', `Basic ${authHeader}`)
+			.expect('Content-Type', /json/)
+			.expect(200);
 
-  	// Victim of the day!
-  	const res = await request(server)
-  		.get(`/v1/ext/nsda/history?nsda_id=4`)
-  		.set('Accept', 'application/json')
-  		.set('Authorization', `Basic ${authHeader}`)
-  		.expect('Content-Type', /json/)
-  		.expect(200);
-
-  	assert.isObject(res.body, 'Response is an object');
-  	assert.property(res.body, 'personId', 'Response has personId property');
-  	assert.property(res.body, 'student', 'Response has student property');
-  	assert.property(res.body, 'judge', 'Response has judge property');
-  	assert.property(res.body, 'quizzes', 'Response has quizzes property');
-  	assert.equal(res.body.personId, '1', 'Returned the proper NSDA ID for the end user');
-
-  }, 30000);
-
+		assert.isObject(res.body, 'Response is an object');
+		assert.property(res.body, 'personId', 'Response has personId property');
+		assert.property(res.body, 'student', 'Response has student property');
+		assert.property(res.body, 'judge', 'Response has judge property');
+		assert.property(res.body, 'quizzes', 'Response has quizzes property');
+		assert.equal(res.body.personId, '1', 'Returned the proper NSDA ID for the end user');
+	}, 30000);
 
 	it('Errors on a missing person id', async () => {
 		await request(server)
