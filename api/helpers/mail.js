12cf31770d4d79f355731eaee957a3e27a034307
Chris Palmer <palmer@tabroom.com>
2026-05-15 14:41:13 -0400

    Try this

--- api/helpers/mail.js
+++ api/helpers/mail.js
@@ -80,7 +80,6 @@ export const emailBlast = async (inputData) => {
 	messageData.bcc = Array.from(new Set(messageData.email));
 	messageData.to = messageData.to || config.MAIL_FROM;
 	messageData.from = messageData.from || config.MAIL_FROM;
-	messageData.replyTo = messageData.replyTo || messageData.from;
 	messageData.subject = messageData.subject || 'Message from Tab';
 	messageData.subject = `[TAB] ${messageData.subject}`;
 
