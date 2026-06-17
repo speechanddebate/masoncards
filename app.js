a737cfd48e026846aecc2d5bcc901b2903b16a86
Chris Palmer <palmer@tabroom.com>
2026-06-17 17:35:48 -0400

    lint

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import expressWinston from 'express-winston';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { initialize } from 'express-openapi';
+// eslint-disable-next-line import/no-unresolved
 import { v7 as uuid } from 'uuid';
 import swaggerUI from 'swagger-ui-express';
 import config from './config/config.js';
